851e7decd768005cf014f6cc172b0bee414ab19d
kevinsmba <kevinsmba@gmail.com>
2023-12-12 18:15:22 -0500

    changes to set up tags

--- cypress/pluguins/index.js
+++ cypress/pluguins/index.js
@@ -1,4 +1,5 @@
-const cucumber = require("@badeball/cypress-cucumber-preprocessor").default;
+/*const cucumber = require("@badeball/cypress-cucumber-preprocessor").default;
+
 
 module.exports = (on, config) => {
   on("file:preprocessor", cucumber({
@@ -7,9 +8,19 @@ module.exports = (on, config) => {
   }));
   return config;
 };
+*/
 
 
 
 
 
+const cucumber = require("@badeball/cypress-cucumber-preprocessor").default;
+const cypressTags = require("cypress-tags"); // import the cypress-tags plugin
 
+module.exports = (on, config) => {
+  on("file:preprocessor", cucumber({
+    // Change the step_definitions path to your desired directory
+    step_definitions: "cypress/e2e/step_definitions",
+  }));
+  return cypressTags(config); // call the cypress-tags plugin with the config object
+};
